842a98033d1440b3911bac07c93e49b70900fbfb
zhouhangshuhai <zhouhangshuhai@sina.com>
2022-05-18 14:54:36 +0800

    Update matrix.cpp
    矩阵的转置和矩阵的乘法

--- matrix.cpp
+++ matrix.cpp
@@ -1,42 +1,41 @@
 //
-// Created by 周舟 on 2022/3/31.
+// Created by 周舟 on 2022/5/18.
 //
-#include<iostream>
+
+
+#include "iostream"
 
 using namespace std;
 
 int main() {
-    int num[50];
-    int i, j, k, m, n;
-    int *p;
-    cout << endl << "输入人数：" << endl;
-    cin >> n;
-    p = num;
-    for (i = 0; i < n; i++) {
-        *(p + i) = i + 1;
-    }
-    i = 0;
-    k = 0;
-    m = 0;
-    while (m < n - 1)
-    {
-        if (*(p + i) != 0) {
-            k++;
-        }
-        if (k == 3)
-        {
-            *(p + i) = 0;
-            k = 0;
-            m++;
-        }
-        i++;
-        if (i == n) {
-            i = 0;
+    // Transform Matrix
+    int a[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    int b[4][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            b[j][i] = a[i][j];
         }
     }
-    while (*p == 0) {
-        p++;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << "最后一个是" << *p << "号" << endl;
-    return 0;
+    // Matrix Multiplication
+    int c[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < 4; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
